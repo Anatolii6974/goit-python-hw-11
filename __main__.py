a712ed400d09c34392b1ac28cbfac37fa1f6a65f
Anatolii6974 <anatolii.perfilov@gmail.com>
2023-07-25 22:49:29 +0300

    first commit

---
+++ __main__.py
@@ -0,0 +1,25 @@
+from Bot import Bot
+from AddressBook import *
+
+
+if __name__ == "__main__":
+    print('Hello. I am your contact-assistant. What should I do with your contacts?')
+    bot = Bot()
+    bot.book.load("auto_save")
+    #commands = ['Add', 'Search', 'Edit', 'Load', 'Remove', 'Save', 'Congratulate', 'View', 'Exit']
+    while True:
+        action = input('Type help for list of commands or enter your command\n').strip().lower()
+        if action == 'help':
+            help = CreateHelpReport()
+            help.report()
+
+            action = input().strip().lower()
+            bot.handle(action)
+            if action in ['add', 'remove', 'edit']:
+                bot.book.save("auto_save")
+        else:
+            bot.handle(action)
+            if action in ['add', 'remove', 'edit']:
+                bot.book.save("auto_save")
+        if action == 'exit':
+            break
